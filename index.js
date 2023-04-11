391e3aa4032ea5b051d251626e8ca7b1ddd363d1
izayaaaaaaa <izaya.orihara0011@gmail.com>
2023-04-11 09:44:30 +0800

    Manual Addition of Backend Data for Usability Purposes

--- index.js
+++ index.js
@@ -11,9 +11,12 @@ import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
 import postRoutes from './routes/posts.js';
-import { register } from "./controllers/auth.js";
-import { createPost } from "./controllers/posts.js";
+import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js'
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -53,5 +56,12 @@ app.use("/posts", postRoutes);
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+
+        // ADD DATA (ONE TIME USE ONLY)
+        // User.insertMany(users);
+        // Post.insertMany(posts);
+
+    })
     .catch((error) => console.log(`${error} did not connect`));
